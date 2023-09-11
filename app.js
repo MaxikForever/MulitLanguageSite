939f349920f509de45f3cb1344cc34c667691bbb
Maxim <69801190+MaxikForever@users.noreply.github.com>
2023-09-11 19:56:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-
 const userLanguage = navigator.language || navigator.userLanguage;
 
 const defaultLanguage = 'ro';
 
-let selectedLanguage = userLanguage.split('-')[0] || defaultLanguage;
-const supportedLanguages = ['ro', 'ru'];
+let selectedLanguage = userLanguage.toLowerCase().split('-')[0] || defaultLanguage; // Convert to lowercase and then split
 
+const supportedLanguages = ['ro', 'ru'];
 
 if (!supportedLanguages.includes(selectedLanguage)) {
     selectedLanguage = defaultLanguage; // Use the default language if the selected one is not supported
 }
 
-window.location.href = `index_${selectedLanguage}.html`;
+
+// Check if the current page matches the selected language before redirecting
+
 
 function switchToRomanian() {
-    window.location.href = 'index_ro.html';
-    alert("switch");
+
+        window.location.href = 'index_ro.html';
+    
+    
 }
 
 function switchToRussian() {
-    window.location.href = 'index_ru.html';
-    alert("switch")
+    if (selectedLanguage !== 'ru') {
+        window.location.href = 'index_ru.html';
+  
+    }
 }
-
