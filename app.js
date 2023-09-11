3212bd7e1850511100f4a728794a31b503687de3
Maxim <69801190+MaxikForever@users.noreply.github.com>
2023-09-11 19:16:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+
+const userLanguage = navigator.language || navigator.userLanguage;
+
+const defaultLanguage = 'ro';
+
+let selectedLanguage = userLanguage.split('-')[0] || defaultLanguage;
+const supportedLanguages = ['ro', 'ru'];
+
+
+if (!supportedLanguages.includes(selectedLanguage)) {
+    selectedLanguage = defaultLanguage; // Use the default language if the selected one is not supported
+}
+
+window.location.href = `index_${selectedLanguage}.html`;
+
 function switchToRomanian() {
     window.location.href = 'index_ro.html';
 }
